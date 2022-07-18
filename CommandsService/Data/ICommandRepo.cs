a76eb3b57c2b3ee0c199cd2afd22c0b43e043c29
ReshiAdavan <reshiadavan27@gmail,com>
2022-07-18 16:35:28 -0400

    Added Models, DB Context, Repository, DTOs, Automapper profiles, and controller actions for the Command service

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges(); 
+
+        // Platform Related
+        IEnumerable<Platform> GetAllPlatforms(); 
+        void CreatePlatform(Platform plat); 
+        bool PlatformExists(int platformId);
+
+        // Command Related
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId); 
+        void CreateCommand(int platformId, Command command);  
+    }
+}
--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandsService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +28,10 @@ namespace CommandsService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            services.AddScoped<ICommandRepo, CommandRepo>();  
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
@@ -45,11 +49,8 @@ namespace CommandsService
             }
 
             // app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
