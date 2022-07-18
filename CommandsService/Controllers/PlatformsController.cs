a80569c1cadd8656985fda9be412c3e921b00f17
ReshiAdavan <reshiadavan27@gmail,com>
2022-07-18 10:27:25 -0400

    Implemented CommandsService microservice in NETInfra

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+            return Ok("Inbound test of from Platforms Controller");  
+        }
+    }
+}
