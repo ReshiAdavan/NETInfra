ee91931848abfcdf6b622515b57d1203a97dd3cb
ReshiAdavan <reshiadavan27@gmail,com>
2022-07-18 10:29:06 -0400

    Created PlatformService in NETInfra microservice

---
+++ PlatformService/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using PlatformService.Data; 
+using Microsoft.EntityFrameworkCore; 
+using PlatformService.SyncDataServices.Http;
+
+namespace PlatformService
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env; 
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env; 
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_env.IsProduction())
+            {
+                Console.WriteLine("--> Using SQL Server DB"); 
+                services.AddDbContext<AppDbContext>(opt => 
+                    opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn"))); 
+            }
+            else 
+            {
+                Console.WriteLine("--> Using InMem DB"); 
+                services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem")); 
+            }
+
+            services.AddScoped<IPlatformRepo, PlatformRepo>(); 
+            services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>(); 
+            services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
+            });
+
+            Console.WriteLine($"--> CommandService Endpoint {Configuration["CommandService"]}"); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
+            }
+
+            // app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            PrepDb.PrepPopulation(app, env.IsProduction()); 
+        }
+    }
+}
